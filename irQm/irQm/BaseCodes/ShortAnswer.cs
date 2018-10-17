c6d504dc38aa5ee346c4fc154b311dc9ef32e9f0
hsafavi <hossein.safavi@gmail.com>
2018-10-17 11:43:19 +0330

    shortAnswer changed

--- irQm/irQm/BaseCodes/ShortAnswer.cs
+++ irQm/irQm/BaseCodes/ShortAnswer.cs
@@ -23,11 +23,9 @@ namespace irQm.BaseCodes
             for (int i = 0; i < Answer.Count; i++)
             {
                 string str = Answer[i];
-                if (str.Trim().Replace(" ", "").Replace("‌", "") == UserAnswer.Replace(" ", "").Replace("‌", ""))
+                if (str.Trim().Replace(" ", "").Replace("‌", "").Replace('ي','ی').Replace( 'ك','ک').Replace('آ', 'ا') == UserAnswer.Replace(" ", "").Replace("‌", "").Replace('ي', 'ی').Replace('ك', 'ک').Replace('آ', 'ا'))
                 {
-                    UserAnswer.Replace('ی', 'ي');
-                    UserAnswer.Replace('ک', 'ك');
-                    UserAnswer.Replace('آ', 'ا');
+                   
                     gainedScore = Score;
                     break;
                 }
